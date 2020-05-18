451886404eb62e0e54fdf3353bc97b09a82369c9
Szymon Szałata <szymon.szalata.1@gmail.com>
2020-05-18 16:49:56 +0200

    Lab9

---
+++ Z9/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Z9
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var view = new StudentView();
+            this.DataContext = view;
+            ListBox.ItemsSource = view.List.Students;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void StudentList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Z9/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Z9
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string FullName => Name + LastName;
+        public DateTime Enlisting { get; set; }
+
+        public override string ToString()
+        {
+            return FullName;
+        }
+
+    }
+
+    public class StudentView
+    {
+        public StudentList List { get; set; }
+        public StudentView()
+        {
+            List = new StudentList()
+            {
+                Students = new ObservableCollection<Student>()
+                {
+                    new Student
+                    {
+                        Id = 1,
+                        Name = "Jan",
+                        LastName = "Kowalski",
+                        Enlisting = new DateTime(2000, 1, 2)
+                    },
+                    new Student
+                    {
+                        Id = 1,
+                        Name = "Zenon",
+                        LastName = "Wiertara",
+                        Enlisting = new DateTime(2001, 2, 3)
+                    },
+                    new Student
+                    {
+                        Id = 1,
+                        Name = "Grzegorz",
+                        LastName = "Brzęczyszczykiewicz",
+                        Enlisting = new DateTime(2002, 3, 4)
+                    }
+                }
+            };
+        }
+    }
+    public class StudentList
+    {
+
+        public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student>();
+    }
+}
